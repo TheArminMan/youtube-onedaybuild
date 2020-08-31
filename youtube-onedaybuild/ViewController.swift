e192e102534e3538d7e4654f41cc75fbfcc97be8
Armin Kamsi <arminkamsi@Armins-MacBook-Pro.local>
2020-08-31 12:35:46 -0700

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Armin Kamsi on 8/31/20.
+//  Copyright © 2020 Armin Kamsi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
